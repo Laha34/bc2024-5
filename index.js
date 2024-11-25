4bef0fcae9bcda1da2dfce894b17e66f1122cb35
Denys bc-2024-5 <denys.lahoida@gmail.com>
2024-11-25 13:55:43 +0200

    Done all part

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const fs = require('fs');
 const express = require('express');
 const program = new Command();
 const app = express();
-const multer = require('multer'); 
-const upload = multer({ storage: multer.memoryStorage() });
+const multer = require('multer');
+const upload = multer();
 
 program
   .option('-h, --host <type>', 'server host')
